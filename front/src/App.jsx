d75357e3e81cd07914c4ae26655b3f6aba3d8c22
066516 <ghmamdjeridinabil@gmail.com>
2024-04-27 00:27:58 +0100

    add stream in web

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React, { useEffect, useRef } from "react";
+import io from "socket.io-client";
 
-function App() {
-  const [count, setCount] = useState(0);
+const socket = io("http://192.168.137.1:3000");
+
+export default function App() {
+  const audioRef = useRef(new Audio());
+
+  useEffect(() => {
+    socket.on("audioStream", (audioData) => {
+      // Play received audio data
+      const audioBlob = new Blob([audioData.audioData], { type: "audio/wav" });
+      const audioURL = URL.createObjectURL(audioBlob);
+      audioRef.current.src = audioURL;
+      audioRef.current.play();
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
-    <>
-      <div className="bg-red-500">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <div>Hello, World!</div>
+      <audio ref={audioRef} controls />
+    </div>
   );
 }
-
-export default App;
